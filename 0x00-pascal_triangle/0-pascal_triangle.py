3e13bb4c94d5269a42e00bd07c4699af11691f1d
psych0wh1z <ikeagudike@gmail.com>
2023-08-03 20:01:31 +0100

    Refactor code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,29 +3,6 @@
 """
 
 
-def factorial(n):
-    """
-    Returns the factorial of a number
-
-    Args:
-        n (int): nnumber to be factorized
-    """
-    if n < 2:
-        return 1
-    return n * factorial(n - 1)
-
-
-def combination(n, k):
-    """
-    Calculates each triangle cell using the "n choose k" formula
-
-    Args:
-        n (int): index of cell row in triangle
-        k (int): index of cell column in triangle
-    """
-    return (factorial(n) // (factorial(k) * factorial(n - k)))
-
-
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing a Pascal’s Triangle
@@ -34,12 +11,26 @@ def pascal_triangle(n):
         n (int): number of rows in pascal triangle
 
     Returns:
-        1. List comprehension representation of pascal's triangle of n
+        1. List representation of pascal's triangle of n
         2. Empty list if n <= 0
     """
     triangle = []
 
     if n > 0:
-        triangle = [[combination(nums, num)
-                     for num in range(nums + 1)] for nums in range(n)]
+        # Iterate over triangle height
+        for line in range(n):
+            # Create new empty list to hold ints
+            nums = []
+            # Loop through row size (number of columns)
+            for num in range(line + 1):
+                # Append 1 at beginning and end of row
+                if num == 0 or num == line:
+                    nums.append(1)
+                # Get sum of prev row[[column - 1] + [column]] and append
+                else:
+                    nums.append(triangle[line - 1][num - 1] +
+                                triangle[line - 1][num])
+            # Append list of integers to triangle
+            triangle.append(nums)
+
     return triangle

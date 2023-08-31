14c0f9fc2de2c913d961c29796b078763b43b879
psych0wh1z <ikeagudike@gmail.com>
2023-08-31 11:43:42 +0100

    Refactor script

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import datetime
 import random
 import sys
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""0-stats module reads stdin line by line, computes and prints log metrics
+"""Reads stdin line by line, computes and prints log metrics
 """
 
 import re
@@ -14,56 +14,43 @@ def print_logs(total_size, cache):
         print("{}: {}".format(code, freq))
 
 
-def parse_logs():
-    cache = {}
-    total_size = 0
-    counter = 0
+cache = {}
+total_size = 0
+counter = 0
 
-    try:
-        pattern = (
-            r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s-\s'
-            r'(\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{6}\])\s'
-            r'(\"GET /projects/260 HTTP/1.1\")\s'
-            r'(\d{3})\s(\d+)'
-        )
+try:
+    pattern = (
+        r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s-\s'
+        r'(\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{6}\])\s'
+        r'(\"GET /projects/260 HTTP/1.1\")\s'
+        r'(\d{3})\s(\d+)'
+    )
 
-        # Start loop, counitng up to 10 lines
-        while True:
-            while counter < 10:
-                # Read each line in standard input
-                for line in sys.stdin:
-                    # Match whole log line and capture groups
-                    match = re.match(pattern, line)
+    for line in sys.stdin:
+        # Match whole log line and capture groups
+        match = re.match(pattern, line)
 
-                    if match:
-                        # Store status code of request in cache
-                        status_code = match.group(4)
-                        cache[status_code] = cache.get(status_code, 0)
-                        cache[status_code] += 1
+        if match:
+            # Store status code of request in cache
+            status_code = match.group(4)
+            cache[status_code] = cache.get(status_code, 0)
+            cache[status_code] += 1
 
-                        # Add file size of request in variable
-                        file_size = int(match.group(5))
-                        total_size += file_size
+            # Add file size of request in variable
+            file_size = int(match.group(5))
+            total_size += file_size
 
-                        # Increment counter by 1
-                        counter += 1
+            # Increment counter by 1
+            counter += 1
 
-                    # Break out of for loop on tenth iteration
-                    break
-
+        if counter == 10:
             # Print logs and reset counter
             print_logs(total_size, cache)
             counter = 0
 
-    # End infinite loop on keyboard interruption and print logs
-    # except KeyboardInterrupt:
-    except Exception as err:
-        pass
+# End infinite loop on keyboard interruption and print logs
+except Exception as err:
+    pass
 
-    finally:
-        print_logs(total_size, cache)
-
-
-if __name__ == "__main__":
-    """Call log parser function if module is invoked"""
-    parse_logs()
+finally:
+    print_logs(total_size, cache)

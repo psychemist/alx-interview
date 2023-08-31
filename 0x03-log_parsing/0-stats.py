efba1acd84e5ca7b6cf6177794783d099365ac50
psych0wh1z <ikeagudike@gmail.com>
2023-08-31 10:55:04 +0100

    Refactor code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""0-stats module reads stdin line by line, computes and prints log metrics"""
+"""0-stats module reads stdin line by line, computes and prints log metrics
+"""
 
 import re
 import sys
 
 
-cache = {}
-total_size = 0
-counter = 0
-
-
-def print_logs():
+def print_logs(total_size, cache):
     """Print total file size, and log status codes and their frequencies
     """
     print(f"File size: {total_size}")
@@ -18,42 +14,52 @@ def print_logs():
         print("{}: {}".format(code, freq))
 
 
-try:
-    pattern = (
-        r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s-\s'
-        r'(\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{6}\])\s'
-        r'(\"GET /projects/260 HTTP/1.1\")\s'
-        r'(\d{3})\s(\d+)'
-    )
+def parse_logs():
+    cache = {}
+    total_size = 0
+    counter = 0
 
-    # Start loop, counitng up to 10 lines
-    while True:
-        while counter < 10:
-            # Read each line in standard input
-            for line in sys.stdin:
-                # Match whole log line and capture groups
-                match = re.match(pattern, line)
+    try:
+        pattern = (
+            r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s-\s'
+            r'(\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{6}\])\s'
+            r'(\"GET /projects/260 HTTP/1.1\")\s'
+            r'(\d{3})\s(\d+)'
+        )
 
-                if match:
-                    # Store status code of request in cache
-                    status_code = int(match.group(4))
-                    cache[status_code] = cache.get(status_code, 0)
-                    cache[status_code] += 1
+        # Start loop, counitng up to 10 lines
+        while True:
+            while counter < 10:
+                # Read each line in standard input
+                for line in sys.stdin:
+                    # Match whole log line and capture groups
+                    match = re.match(pattern, line)
 
-                    # Add file size of request in variable
-                    file_size = int(match.group(5))
-                    total_size += file_size
+                    if match:
+                        # Store status code of request in cache
+                        status_code = int(match.group(4))
+                        cache[status_code] = cache.get(status_code, 0)
+                        cache[status_code] += 1
 
-                    # Increment counter by 1
-                    counter += 1
+                        # Add file size of request in variable
+                        file_size = int(match.group(5))
+                        total_size += file_size
 
-                # Break out of for loop on tenth iteration
-                break
+                        # Increment counter by 1
+                        counter += 1
 
-        # Print logs and reset counter
-        print_logs()
-        counter = 0
+                    # Break out of for loop on tenth iteration
+                    break
 
-# End infinite loop on keyboard interruption and print logs
-except KeyboardInterrupt:
-    print_logs()
+            # Print logs and reset counter
+            print_logs(total_size, cache)
+            counter = 0
+
+    # End infinite loop on keyboard interruption and print logs
+    except KeyboardInterrupt:
+        print_logs(total_size, cache)
+
+
+if __name__ == "__main__":
+    """Call log parser function if module is invoked"""
+    parse_logs()

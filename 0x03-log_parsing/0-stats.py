8638ea3fffee600a3e2195ea5b8dbd56184a401f
psych0wh1z <ikeagudike@gmail.com>
2023-08-31 07:03:48 +0100

    Implement log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,34 +5,56 @@
 import re
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+
+cache = {}
 total_size = 0
 counter = 0
 
+
+def print_logs():
+    """Print total file size, and log status codes and their frequencies
+    """
+    print(f"File size: {total_size}")
+    for code, freq in sorted(cache.items()):
+        print("{}: {}".format(code, freq))
+
+
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+    pattern = (
+        r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s-\s'
+        r'(\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{6}\])\s'
+        r'(\"GET /projects/260 HTTP/1.1\")\s'
+        r'(\d{3})\s(\d+)'
+    )
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    # Start loop, counitng up to 10 lines
+    while True:
+        while counter < 10:
+            # Read each line in standard input
+            for line in sys.stdin:
+                # Match whole log line and capture groups
+                match = re.match(pattern, line)
 
-except Exception as err:
-    pass
+                if match:
+                    # Store status code of request in cache
+                    status_code = int(match.group(4))
+                    cache[status_code] = cache.get(status_code, 0)
+                    cache[status_code] += 1
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+                    # Add file size of request in variable
+                    file_size = int(match.group(5))
+                    total_size += file_size
+
+                    # Increment counter by 1
+                    counter += 1
+
+                # Break out of for loop on tenth iteration
+                break
+
+        # Print logs and reset counter
+        print_logs()
+        counter = 0
+
+# End infinite loop on keyboard interruption and print logs
+except KeyboardInterrupt:
+    print_logs()

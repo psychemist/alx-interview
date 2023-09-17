1570f97d384eac0731f6bb0a1e6d4381659834e5
psych0wh1z <ikeagudike@gmail.com>
2023-09-17 13:02:03 +0100

    Add matrix rotatoooor function

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,9 +2,22 @@
 """ Rotate Matric module
 """
 
+
 def rotate_2d_matrix(matrix):
     """ Rotate a 2D matrix in place
     Args:
         (matrix): a non-empty, 2-Dimensional matrix
+    Returns:
+        (List[List[int]]): matrix rotated 90 degrees clockwise
     """
-    for
+    rotated_matrix = []
+
+    for column in range(len(matrix)):
+        matrix_row = []
+        for row in range(len(matrix) - 1, -1, -1):
+            matrix_row.append(matrix[row][column])
+        rotated_matrix.append(matrix_row)
+
+    for row in range(len(rotated_matrix)):
+        for column in range(len(rotated_matrix[0])):
+            matrix[row][column] = rotated_matrix[row][column]

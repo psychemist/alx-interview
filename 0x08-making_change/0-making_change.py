21fe38fdd3361bb23339742ede1958ae3cd074b8
psych0wh1z <ikeagudike@gmail.com>
2023-09-27 23:21:48 +0100

    Update README

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""
-Interview Question on: fewest number of coins needed to
-meet a given amount total
+""" 0-making_change module
 """
 
 
 def makeChange(coins, total):
-    """ fewest number of coins needed to meet total """
+    """Determine the fewest number of coins needed to meet a given amount
+    Args:
+        coins (list): values of the coins in your possession
+        total (int): amount of chnage required
+    Returns:
+        change (int): fewest number of coins needed to meet total
+    """
     if total <= 0:
         return 0
-    # sort the coins in descending order
-    coins.sort(reverse=True)
     change = 0
+
+    # sort coins list in descending order
+    coins.sort(reverse=True)
+
     for coin in coins:
-        if total <= 0:
-            break
-        temp = total // coin
-        change += temp
-        total -= (temp * coin)
-    if total != 0:
-        return -1
-    return change
+        while total >= coin:
+            total -= coin
+            change += 1
+
+    if total == 0:
+        return change
+    return -1
--- 0x08-making_change/README.md
+++ 0x08-making_change/README.md
@@ -1,1 +1,1 @@
-# Making Change Algo
+# Making Change
